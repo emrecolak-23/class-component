b6a5444472319d6492e347351825dd390709dc57
Emre Çolak <emrecolak@Emre.home>
2023-01-25 21:45:56 +0300

    initializing state through constructor

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import SeasonDisplay from './SeasonDisplay';
+import { Component } from 'react';
 
-function App() {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+  }
 
-  return (
-    <div>
-      <SeasonDisplay />
-    </div>
-  );
+  // React says we have to define render!!
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+
+    return <div>Lattitude: </div>;
+  }
 }
 
 export default App;
